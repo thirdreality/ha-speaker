ccd2852d7b4b4b31f103b1bef4eb40ee2e0b0a2d
tianwei.ge <tianwei.ge@thirdreality.ai>
2026-03-27 14:00:03 +0800

    expand the system partition to 200 MB, and recreate the partition after the OTA update

--- buildroot/board/thirdreality/trspk/post_build.sh
+++ buildroot/board/thirdreality/trspk/post_build.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# find "$TARGET_DIR/usr/lib/python3.11/site-packages/numpy" \
-#     -type d \( -name tests -o -name '__pycache__' \) \
-#     -prune -exec rm -rf {} +
-
 rm -rf $TARGET_DIR/usr/lib/python3.11/site-packages/pymicro_wakeword/models
 
 exit 0
